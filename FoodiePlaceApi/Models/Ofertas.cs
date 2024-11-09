bb22f745f3b962158be0d0f52f640a03d633de75
HenzoWTF <Urenahenzo@gmail.com>
2024-11-09 18:50:49 -0400

    Crea Ofertas y Tarjetas

---
+++ FoodiePlaceApi/Models/Ofertas.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodiePlaceApi.Models
+{
+    public class Ofertas
+    {
+        [Key]
+        public int OfertasId { get; set; }
+
+        [ForeignKey("Productos")]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser un número positivo.")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "El descuento es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El descuento debe ser un número positivo.")]
+        public decimal Descuento { get; set; }
+
+        [Required(ErrorMessage = "El precio de oferta es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio de oferta debe ser un número positivo.")]
+        public decimal PrecioOferta { get; set; }
+
+        [Required]
+        public DateTime FechaInicio { get; set; }
+
+        [Required]
+        public DateTime FechaFinal { get; set; }
+
+        public byte[]? Imagen { get; set; }
+    }
+}
---
+++ FoodiePlaceApi/Models/Tarjetas.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodiePlaceApi.Models;
+
+public class Tarjetas
+{
+    [Key]
+    public int TarjetaId { get; set; }
+
+
+    [ForeignKey("Usuarios")]
+    public int UsuarioId { get; set; }
+
+    [Required]
+    public string TipoTarjeta { get; set; }
+
+    [Required]
+    [StringLength(16, MinimumLength = 13)] // Limita la longitud del número de tarjeta entre 13 y 16 caracteres
+    [RegularExpression(@"^\d+$", ErrorMessage = "El número de tarjeta debe contener solo dígitos.")]
+    public string NumeroTarjeta { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime FechaExpiracion { get; set; }
+
+    [Required]
+    [StringLength(3, MinimumLength = 3)] // Limita la longitud del CVV a 3 caracteres
+    [RegularExpression(@"^\d+$", ErrorMessage = "El CVV debe contener solo dígitos.")]
+    public string CVV { get; set; }
+}
